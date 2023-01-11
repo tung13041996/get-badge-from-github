c33c8c99ba17b0eaf8f177b921f8703dbcd4111e
Tung Nguyen <tung.nguyen@viivue.com>
2023-01-11 09:49:04 +0700

    init sources

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Get Badge From Github</h1>
+    <form @submit.prevent="sendForm">
+      <div class="field-wrapper">
+        <label>Input Name Repository</label>
+        <input type="text" placeholder="Repository" />
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    sendForm() {},
+  },
+};
+</script>
+
+<style scoped></style>
